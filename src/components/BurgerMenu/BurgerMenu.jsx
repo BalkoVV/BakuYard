36cd3c2b3aefae262fc5c53b3faa8efe78f5f450
ViktorBalko <ben.happyngton@gmail.com>
2024-05-08 13:03:53 +0300

    5

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -36,7 +36,7 @@ const BurgerMenu = ({ isOpen, onClose }) => {
 
       {isOpen && <div className={styles.backdrop} onClick={onClose}></div>}
       <div ref={menuRef} className={`${styles.burgerMenu} ${isOpen ? styles.isOpen : ''}`}>
-      <div className={styles.borderDecorative}></div>
+      {/* <div className={styles.borderDecorative}></div> */}
 
         <ul>
           <li><NavLink exact to="/" onClick={onClose} activeClassName={styles.activeLink}>Головна</NavLink></li>
