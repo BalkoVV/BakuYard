35b714f41e17d4725db8c1c31b40e2643853133e
ViktorBalko <ben.happyngton@gmail.com>
2024-05-09 19:14:57 +0300

    14

--- src/components/MenuComponents/Meat.jsx
+++ src/components/MenuComponents/Meat.jsx
@@ -1,10 +1,11 @@
 // Meet.jsx
 
 import React, { useEffect, useState, useContext } from "react";
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import AddButton from 'components/MenuComponents/AddButton/AddButton';
-import ToMenuButton from "components/ToMenuButton/ToMenuButton";
+// import ToMenuButton from "components/ToMenuButton/ToMenuButton";
+import { ReactComponent as ToMenuIcon } from 'images/left.svg';
 
 import { CartContext } from "components/CartProvider/CartProvider";
 
@@ -53,7 +54,13 @@ export const Meat = () => {
       
       <div className={styles.discriptionBox}>
           <h1 className={styles.dishTitle}>М'ясо</h1> 
-          <ToMenuButton className={styles.buttonToMenu}/>
+          {/* <ToMenuButton className={styles.buttonToMenu}/> */}
+          <div className={styles.toMenuButton}>
+       <NavLink to="/menu" className={styles.toMenuLink}>
+         <ToMenuIcon className={styles.toMenuIcon}/>
+         <h1 className={styles.toMenuDescription}>до Менюс</h1>
+       </NavLink>
+      </div>
       </div>
      
       <div className={styles.dishPageContainer}>
