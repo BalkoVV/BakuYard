8d7727159b83659914f6e2c5341f27cf962d94fb
ViktorBalko <ben.happyngton@gmail.com>
2024-05-17 13:54:25 +0300

    check after setup Custom Domain

--- src/components/InMapButton/InMapButton.jsx
+++ src/components/InMapButton/InMapButton.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import styles from 'components/InMapButton/InMapButton.module.css';
-import flagMapIcon from 'images/flagMap.svg';
+// import flagMapIcon from 'images/flagMap.svg';
 
 export const InMapButton = () => {
    return (
       <div className={styles.inMapButton}>
          
-         <span data-tooltip="Ми на карті" data-flow="right">
+         {/* <span data-tooltip="Ми на карті" data-flow="right"> */}
             
          <a
             href="https://www.google.com/maps/place/%D0%91%D0%B0%D0%BA%D0%B8%D0%BD%D1%81%D1%8C%D0%BA%D0%B8%D0%B9+%D0%94%D0%B2%D1%96%D1%80/@50.5090683,30.7926622,19.96z/data=!4m14!1m7!3m6!1s0x40d4d964534977a3:0x85bce3f2aa0b7a83!2z0JHQsNC60LjQvdGB0YzQutC40Lkg0JTQstGW0YA!8m2!3d50.5090529!4d30.7929779!16s%2Fg%2F11b76tsmxr!3m5!1s0x40d4d964534977a3:0x85bce3f2aa0b7a83!8m2!3d50.5090529!4d30.7929779!16s%2Fg%2F11b76tsmxr?entry=ttu"
@@ -16,20 +16,20 @@ export const InMapButton = () => {
             rel="noreferrer"
             className={styles.inMapLink}
          >
-            <img
+            {/* <img
         src={flagMapIcon}
         alt=""
         style={{
         width: '60px',
         height: '60px',
         }}
-        />
-            
+        /> */}
+            <h3>Ми на карті</h3>
          </a>
             {/* CSS Tooltips */}
             
-            </span>
-            <h3>Бакінський Двір</h3>
+            {/* </span> */}
+            {/* <h3>Бакінський Двір</h3> */}
       </div>
    );
 };
