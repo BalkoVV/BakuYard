f751aa63dd86c2b37286dbd22d2620efb655dcdd
ViktorBalko <ben.happyngton@gmail.com>
2024-05-19 18:28:05 +0300

    2

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -19,7 +19,7 @@ export const ContactsPage = () => {
       contentElements.forEach((element, index) => {
         setTimeout(() => {
           element.style.opacity = '1'; 
-        }, 10 * index); 
+        }, 1 * index); 
       });
     }, []); 
 
@@ -28,35 +28,7 @@ export const ContactsPage = () => {
 
       <div className={styles.mapBox}>
         <InMapButton className={styles.inMap}/>
-        {/* <img
-        src={flagMapIcon}
-        alt=""
-        style={{
-        position: 'absolute',
-        top: '235px',
-        left: '715px',
-        width: '60px',
-        height: '60px',
-        }}
-        /> */}
-        {/* <div className={styles.inMapButton}>
-         <span data-tooltip="Ми на карті" data-flow="right">
-            
-         <a
-            href="https://www.google.com/maps/place/%D0%91%D0%B0%D0%BA%D0%B8%D0%BD%D1%81%D1%8C%D0%BA%D0%B8%D0%B9+%D0%94%D0%B2%D1%96%D1%80/@50.5090683,30.7926622,19.96z/data=!4m14!1m7!3m6!1s0x40d4d964534977a3:0x85bce3f2aa0b7a83!2z0JHQsNC60LjQvdGB0YzQutC40Lkg0JTQstGW0YA!8m2!3d50.5090529!4d30.7929779!16s%2Fg%2F11b76tsmxr!3m5!1s0x40d4d964534977a3:0x85bce3f2aa0b7a83!8m2!3d50.5090529!4d30.7929779!16s%2Fg%2F11b76tsmxr?entry=ttu"
-            
-            target="_blank"
-            rel="noreferrer"
-            className={styles.inMapLink}
-         >
-            
-            
-         </a>
-         
-            
-            </span>
         
-      </div> */}
       </div>
 
         <div className={styles.contactsPageContainer}>
